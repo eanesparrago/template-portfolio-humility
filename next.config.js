f686bdc5d20cc088b53812dc723bdd79377e72ae
LJEsp <esparragoleandroj@gmail.com>
2019-02-02 21:17:46 +0800

    Test

--- next.config.js
+++ next.config.js
@@ -22,6 +22,6 @@ module.exports = {
     }
     // This will copy _redirects from project root into the out directory
     await copyFile(join(dir, "_redirects"), join(outDir, "_redirects"));
-    return defaultPathMap;
+    return map;
   }
 };
