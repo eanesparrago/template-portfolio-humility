3b5dae532212504b12e2ae5b8d0c7f1c2efb663d
LJEsp <esparragoleandroj@gmail.com>
2019-02-05 22:37:00 +0800

    Change photos

--- src/data/projects.js
+++ src/data/projects.js
@@ -5,10 +5,11 @@ export default [
     date: "February 2019",
     title: "Title Placeholder",
     subtitle: "Subtitle Placeholder",
-    description: "Deserunt duis sit culpa do cillum cupidatat.",
+    description: "Amet sint reprehenderit amet enim veniam irure duis.",
     thumbnail: {
-      link: "https://via.placeholder.com/350x150",
-      description: "thumbnail"
+      link:
+        "https://images.unsplash.com/photo-1527427337751-fdca2f128ce5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      description: "Thumbnail"
     },
     body:
       "Officia adipisicing aliqua aute proident pariatur est Lorem mollit cupidatat ullamco. Eu dolor esse nisi deserunt aliqua mollit est qui deserunt esse. Laboris ad qui consectetur cupidatat laborum nisi excepteur dolor ea dolore.",
@@ -43,8 +44,9 @@ export default [
     subtitle: "Subtitle Placeholder",
     description: "Amet sint reprehenderit amet enim veniam irure duis.",
     thumbnail: {
-      link: "https://via.placeholder.com/350x150",
-      description: "thumbnail"
+      link:
+        "https://images.unsplash.com/photo-1484807352052-23338990c6c6?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      description: "Thumbnail"
     },
     body:
       "Ex aliqua duis excepteur consectetur reprehenderit irure eiusmod. Nulla exercitation tempor cupidatat ullamco adipisicing fugiat eu dolor. Excepteur reprehenderit id quis nisi ipsum officia sint magna pariatur.",
@@ -79,8 +81,9 @@ export default [
     subtitle: "Subtitle Placeholder",
     description: "Commodo nisi do minim voluptate.",
     thumbnail: {
-      link: "https://via.placeholder.com/350x150",
-      description: "thumbnail"
+      link:
+        "https://images.unsplash.com/photo-1497215842964-222b430dc094?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      description: "Thumbnail"
     },
     body:
       "Et tempor do consequat magna elit adipisicing dolor eiusmod commodo. Et eu sit sit labore exercitation anim anim. Fugiat tempor exercitation Lorem commodo eiusmod labore laboris proident aliquip esse eu magna. Eu pariatur nisi Lorem consequat occaecat ad fugiat nulla et elit ex velit. Ut proident velit dolor Lorem ea sit velit magna eu ullamco deserunt adipisicing.",
@@ -118,7 +121,7 @@ export default [
       "Cupidatat incididunt irure laborum in et velit esse consequat exercitation sunt adipisicing eiusmod quis nostrud. In pariatur in sint commodo exercitation nulla velit cillum nostrud excepteur. Lorem veniam deserunt in tempor occaecat aute amet pariatur Lorem ea velit minim. Culpa irure qui quis aliqua aliquip id aliqua consequat. Esse adipisicing ullamco occaecat non nisi veniam duis anim velit et. Ea deserunt deserunt amet velit officia quis aliquip ut elit occaecat. Est cillum sint mollit laboris minim reprehenderit esse consectetur amet proident laboris.",
     technologies: ["React", "Node.js", "Sass", "HTML", "CSS", "JavaScript"],
     thumbnail: {
-      link: "https://via.placeholder.com/350x150",
+      link: "https://images.unsplash.com/photo-1499175860898-fa38bee4bcdd?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       description: "thumbnail"
     },
     photos: [
@@ -154,7 +157,8 @@ export default [
       "Eiusmod minim laboris nostrud magna officia nostrud qui. Esse amet incididunt qui velit proident non. Anim veniam do minim aliquip labore proident veniam irure veniam irure anim laborum dolore cillum.",
     technologies: ["React", "Node.js", "Sass", "HTML", "CSS", "JavaScript"],
     thumbnail: {
-      link: "https://via.placeholder.com/350x150",
+      link:
+        "https://images.unsplash.com/photo-1504887607625-fac9cade94b7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
       description: "thumbnail"
     },
     photos: [
